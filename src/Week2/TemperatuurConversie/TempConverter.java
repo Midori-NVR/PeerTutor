ebf358d927e245e8d26e5083e25fbb94af7589a9
Midori <niels.van.reeth@gmail.com>
2019-09-24 20:52:47 +0200

    Initial commit

---
+++ src/Week2/TemperatuurConversie/TempConverter.java
@@ -0,0 +1,11 @@
+package Week2.TemperatuurConversie;
+
+/**
+ * @author Niels Van Reeth
+ * @version 1.0 24/09/2019.
+ */
+public class TempConverter {
+    public static void main(String[] args) {
+
+    }
+}
